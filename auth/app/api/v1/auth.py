3dd630044568b1d29d306b3bbca32c95a63e9e28
Oleg Ilin <ilyinon@mail.ru>
2024-10-12 02:51:00 +0300

    dev_oauth-google-base-version

--- auth/app/api/v1/auth.py
+++ auth/app/api/v1/auth.py
@@ -12,8 +12,15 @@ from schemas.user import UserCreate, UserResponse
 from services.auth import AuthService, get_auth_service
 from services.session import SessionService, get_session_service
 from services.user import UserService, get_user_service
+from urllib.parse import urlencode, parse_qs
+from fastapi.responses import RedirectResponse
+from starlette.datastructures import URL
+from starlette.config import Config
+from fastapi.security import OAuth2PasswordBearer
+
 
 get_token = HTTPBearer(auto_error=False)
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 router = APIRouter()
 
@@ -105,6 +112,104 @@ async def login(
         status_code=status.HTTP_401_UNAUTHORIZED, detail="Bad username or password"
     )
 
+
+@router.get(
+    "/login/google",
+    summary="OAuth w/google",
+    response_model=None,
+        responses={
+        "401": {"model": HTTPExceptionResponse},
+        "403": {"model": HTTPExceptionResponse},
+        "422": {"model": HTTPValidationError},
+    },
+    tags=["Authorization"],
+    )
+async def google_login(request: Request):
+    redirect_uri = str(URL(scheme="http", host=request.headers["host"], path="/login/google/callback"))
+    params = {
+        "client_id": config("GOOGLE_CLIENT_ID"),
+        "redirect_uri": redirect_uri,
+        "response_type": "code",
+        "scope": "openid email profile",
+    }
+    url = URL(scheme="https", host="accounts.google.com", path="/o/oauth2/v2/auth")
+    url = url.with_query(urlencode(params))
+    return RedirectResponse(url=str(url))
+
+
+@router.get(
+        "/login/google/callback",
+        summary="callback for google",
+        response_model=None,
+        responses={
+        "401": {"model": HTTPExceptionResponse},
+        "403": {"model": HTTPExceptionResponse},
+        "422": {"model": HTTPValidationError},},
+    tags=["Authorization"],
+    )
+async def google_auth_callback(
+    request: Request):
+    code = request.query_params.get("code")
+    if not code:
+        raise HTTPException(status_code=400, detail="Missing code")
+
+
+    # client_id = config("GOOGLE_CLIENT_ID")
+    # client_secret = config("GOOGLE_CLIENT_SECRET")
+    redirect_uri = str(URL(scheme="http", host=request.headers["host"], path="/login/google/callback"))
+
+    # params = {
+    #     "client_id": client_id,
+    #     "client_secret": client_secret,
+    #     "code": code,
+    #     "grant_type": "authorization_code",
+    #     "redirect_uri": redirect_uri
+    # }
+
+    # response = await http.post(
+    #     "https://oauth2.googleapis.com/token", 
+    #     data=params,
+    #     headers={"Content-Type": "application/x-www-form-urlencoded"}
+    # )
+    # response.raise_for_status()
+    # data = await response.json()
+    # access_token = data.get("access_token")
+    # if not access_token:
+    #     raise HTTPException(status_code=400, detail="Error retrieving access token")
+
+
+    # user_info_response = await http.get(
+    #     "https://www.googleapis.com/oauth2/v3/userinfo",
+    #     headers={"Authorization": f"Bearer {access_token}"}
+    # )
+
+    # user_info_response.raise_for_status()
+    # user_info = await user_info_response.json()
+
+    # email = user_info.get("email")
+    # user = db.query(User).filter(User.email == email).first()
+    # if not user:
+    #     user = User(email=email)
+    #     db.add(user)
+    #     db.commit()
+    #     db.refresh(user)
+
+
+    # access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    # access_token = create_access_token(
+    #     data={"sub": user.email}, expires_delta=access_token_expires
+    # )
+
+
+    # response = RedirectResponse(url="/", status_code=302)
+    # response.set_cookie(
+    #     key="access_token",
+    #     value=access_token,
+    #     httponly=True,
+    #     secure=True,
+    #     samesite="lax"
+    # )
+    # return response
 
 @router.post(
     "/logout",
@@ -218,32 +323,41 @@ async def refresh_tokens(
     responses={
         "401": {"model": HTTPExceptionResponse},
         "403": {"model": HTTPExceptionResponse},
-        "422": {"model": HTTPValidationError},
+        "422": {"model": HTTPExceptionResponse},
     },
     tags=["Authorization"],
 )
 async def check_access(
     request: Request,
     access_token: str = Depends(get_token),
-    allow_roles: Literal["admin", "user"] = None,
+    allow_roles: Optional[Literal["admin", "user"]] = None,
     auth_service: AuthService = Depends(get_auth_service),
 ) -> Optional[Union[HTTPExceptionResponse, HTTPValidationError]]:
     """
     check access.
     """
+    logger.info(f"!!!!!!! {access_token} !!!!!!!!")
     if access_token:
-        logger.info(f"Check access for {access_token.credentials}")
+        logger.info(f"Check access for {access_token}")
 
+        # check access with roles
         if allow_roles:
-            if await auth_service.check_access_with_roles(
-                creds=access_token.credentials, allow_roles=allow_roles
-            ):
+            is_authorized = await auth_service.check_access_with_roles(
+                access_token.credentials, allow_roles
+            )
+            if is_authorized:
                 return status.HTTP_200_OK
-        if not allow_roles:
-            if await auth_service.check_access(creds=access_token.credentials):
+            
+        #check access w/o roles
+        else:
+            is_authorized = await auth_service.check_access(access_token.credentials)
+            if is_authorized:
                 return status.HTTP_200_OK
 
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized"
+            status_code=status.HTTP_403_FORBIDDEN, detail="Access denied"
         )
-    raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized"
+    )
