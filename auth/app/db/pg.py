6f73a1c5d12b87cac7715fee7e49d6f4518fac5a
Oleg Ilin <ilyinon@mail.ru>
2024-10-15 22:28:11 +0300

    add reate limits to nginx

--- auth/app/db/pg.py
+++ auth/app/db/pg.py
@@ -1,15 +1,24 @@
 from core.config import auth_settings
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,)
 from sqlalchemy.orm import declarative_base
-
-from sqlalchemy.ext.asyncio import create_async_engine  # isort: skip
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker  # isort: skip
 
 Base = declarative_base()
 
 engine = create_async_engine(
-    auth_settings.database_dsn, echo=auth_settings.pg_echo, future=True
+    auth_settings.database_dsn,
+    echo=auth_settings.pg_echo,
+    future=True,
+    pool_size=10,
+    max_overflow=20,
+    pool_timeout=30,
+    pool_recycle=600,
 )
-async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+async_session = async_sessionmaker(
+    bind=engine, class_=AsyncSession, expire_on_commit=False
+)
 
 
 async def get_session() -> AsyncSession:
