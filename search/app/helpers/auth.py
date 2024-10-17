ba9500295f9c15588d7fe7478842227560e2349f
Tamara Savadyan <attamaras@Tamaras-MacBook-Pro.local>
2024-10-17 15:27:11 +0300

    reworked to use decorator for checking access

--- search/app/helpers/auth.py
+++ search/app/helpers/auth.py
@@ -1,12 +1,16 @@
+from functools import wraps
+from typing import Callable, List
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBearer
 import httpx
 from core.config import settings
 
+get_token = HTTPBearer(auto_error=False)
 
 async def check_from_auth(
     allow_roles: list,
     credentials: str,
 ) -> bool:
-    # return True
     if credentials:
         token = credentials.credentials
         headers = {"Authorization": f"Bearer {token}"}
@@ -20,3 +24,15 @@ async def check_from_auth(
             if response.status_code == 200:
                 return True
     return False
+
+# Decorator to check user's access
+def roles_required(roles_list: List[str]):
+    def decorator(func: Callable):
+        @wraps(func)
+        async def wrapper(*args, credentials: str = Depends(get_token), **kwargs):
+            access_granted = await check_from_auth(roles_list, credentials)
+            if not access_granted:
+                raise HTTPException(status_code=403, detail="You are not authorized to access this resource")
+            return await func(*args, **kwargs)
+        return wrapper
+    return decorator
